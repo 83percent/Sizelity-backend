c53539c5671f813585aa67461d76a7d3a4a7221d
JaeHoon Lee <hoonni2709@naver.com>
2021-02-24 20:16:49 +0900

    83percent

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-import http from 'http';
+const http = require('http');
+const url = require("url");
+const fs = require('fs');
+
+const app = http.createServer((request, response) => {
+    let _url = request.url;
+    let f = null;
+    const queryData = url.parse(_url, true).query;
+    if(_url === '/'){
+        f = '/index.html';
+    } else {
+        f = '/wrong.html';
+    }
+    if(_url === '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
+    console.log(__dirname);
+    response.end(fs.readFileSync(__dirname + f));
+});
+app.listen(3001);
