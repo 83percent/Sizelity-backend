1a7f284e32cb9539fe8188e5214e3d94a36bead0
JaeHoon Lee <hoonni2709@naver.com>
2021-09-16 00:39:32 +0900

    서버리스 포기

--- lambda.js
+++
@@ -1,6 +0,0 @@
-// lambda.js
-require('source-map-support/register')
-const serverlessExpress = require('@vendia/serverless-express')
-const app = require('./app')
-
-exports.handler = serverlessExpress({ app })
--- server.local.js
+++
@@ -1,5 +0,0 @@
-const app = require('./server');
-const port = 3001;
-
-app.listen(port);
-console.info(`listening on http://localhost:${port}`)
